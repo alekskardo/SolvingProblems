e8ebaefbc22241393b54ed70b954e57dfde7cb2c
Aleksandar <aleksandaralavanja@hotmail.com>
2021-10-17 20:07:56 +0200

    coderbyte

--- 09-10-2021/script.js
+++ 09-10-2021/script.js
@@ -46,3 +46,15 @@
 // console.log(LongestWord("fun&!!, Time"));
 
 // 4. *****************************************
+// First Reverse
+// Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+
+// Examples
+// Input: "coderbyte"
+// Output: etybredoc
+
+// function FirstReverse(str) {
+//   const reverseString = str.split('').reverse().join('');
+//   return reverseString;
+//   // code goes here
+// }
