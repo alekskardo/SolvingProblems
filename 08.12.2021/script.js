e6ec1ba29f237134f34bef3181626826226b115c
Aleksandar <aleksandaralavanja@hotmail.com>
2021-12-08 23:01:53 +0100

    removing element

---
+++ 08.12.2021/script.js
@@ -0,0 +1,16 @@
+// Remove Every nth Element from an Array
+
+"use strict";
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+// for (let i = 2; i < nums.length; i+=3) {
+//         delete nums[i];
+// }
+// nums = nums.filter(item => item)
+
+// console.log(nums);
+// console.log(nums.length);
+
+let newArray = nums.filter((item, idx) => (idx + 1) % 3 !== 0);
+
+console.log(newArray);
